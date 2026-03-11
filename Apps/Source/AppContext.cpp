e8d9b6c46e140b36d74bf4503205b024067d8ba8
Itsmeakash248 <145587932+Itsmeakash248@users.noreply.github.com>
2026-03-11 18:44:03 +0530

    refactor: Update AppContext to hold AppManifest by value instead of a pointer.

--- Apps/Source/AppContext.cpp
+++ Apps/Source/AppContext.cpp
@@ -1,11 +1,9 @@
 #include <flx/apps/AppContext.hpp>
-#include <flx/apps/AppManifest.hpp>
 
 namespace flx::apps {
 
 const std::string& AppContext::getAppId() const {
-	static const std::string empty;
-	return m_manifest ? m_manifest->appId : empty;
+	return m_manifest.appId;
 }
 
 AppPaths AppContext::getPaths() const {
